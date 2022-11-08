bae06d522f8397371846a190833ee3a95a2ba681
krishna306 <krishnakumarkeer5017@gmail.com>
2022-11-08 20:10:04 +0530

    error fix

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -4,10 +4,11 @@ const NewsArticle = require("../models/NewsArticle");
 
 //get news by category
 
-router.get("/fetchnewsbycategory/:id", async (req, res) => {
-  console.log("hey");
+router.get("/fetchnewsbycategory/id", async (req, res) => {
+  
   try {
     const {category} = req.params;
+    console.log(category);
     const articles = await NewsArticle.find({category:category});
     res.json(articles);
   } catch (error) {
